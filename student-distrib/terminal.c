38c39473de3a922468fac7d537d796ae979fc2ed
Jones <caj2@illinois.edu>
2020-10-18 15:55:31 -0500

    minor change

--- student-distrib/terminal.c
+++ student-distrib/terminal.c
@@ -31,6 +31,7 @@ int32_t terminal_open(const uint8_t* filename)
   clear_buffer(); // Sets keyboard_buffer and key_index
 
   /* Turn on Keyboard IRQ */
+  // enable_irq(1);
 
   /* Intiialize variables */
   enter_down = 0;
@@ -228,7 +229,7 @@ int32_t keyboard_handler(void)
     /* Load keyboard buffer with symbol */
     key_buffer[key_index] = input;
 
-    /* Update index in keyboard */
+    /* Update index in keyboard buffer */
     key_index++;
 
     /* Close critical section */
@@ -237,6 +238,6 @@ int32_t keyboard_handler(void)
   }
 
   /* Send interrupt signal for keyboard, the first IRQ */
-  send_eoi(1);
+  //send_eoi(1);
   return 0;
 }
