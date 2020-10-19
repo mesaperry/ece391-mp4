ec037472059d1dd454e6e12b00527a4943f55711
Perry <mesagp2@illinois.edu>
2020-10-19 18:00:51 -0500

    add test to rtc handler

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -54,6 +54,9 @@ void rtc_handler() {
   outb(REG_C, RTC_PORT);
   inb(CMOS_PORT);
 
+  /* Checkpoint 1 RTC test */
+  test_interrupts();
+
   /* Send end of interrupt signal */
   send_eoi(RTC_IRQ);
 }
