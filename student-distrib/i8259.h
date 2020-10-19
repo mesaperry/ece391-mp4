667f136e3ba584230b44db4d13382911328398ba
Jones <caj2@illinois.edu>
2020-10-18 23:24:44 -0500

    update

--- student-distrib/i8259.h
+++ student-distrib/i8259.h
@@ -9,24 +9,27 @@
 #include "types.h"
 
 /* Ports that each PIC sits on */
-#define MASTER_8259_PORT    0x20
-#define SLAVE_8259_PORT     0xA0
+#define MASTER_8259_PORT_ADD    0x20
 #define MASTER_8259_PORT_DAT    0x21
+#define SLAVE_8259_PORT_ADD     0xA0
 #define SLAVE_8259_PORT_DAT     0xA1
-
-/* IRQ related variable */
-#define 	IRQ_AMNT	8
-#define		IRQ2 		2			// Masking IRQ2 will stop the slave PIC from raising interrupts
 
 /* Initialization control words to init each PIC.
  * See the Intel manuals for details on the meaning
- * of each word */
-#define ICW1                0x11
-#define ICW2_MASTER         0x20
-#define ICW2_SLAVE          0x28
-#define ICW3_MASTER         0x04
-#define ICW3_SLAVE          0x02
-#define ICW4                0x01
+ * of each word
+ *
+ * https://courses.grainger.illinois.edu/ece391/fa2020/secure/references/8259A_PIC_Datasheet.pdf
+ * Page 9 - 12
+ */
+#define ICW1                0x11 /* Starts the intialization sequence */
+
+#define ICW2_MASTER         0x20 /* Handles page starting address of */
+#define ICW2_SLAVE          0x28 /* certain routines */
+
+#define ICW3_MASTER         0x04 /* Read when there's more than on 8529 */
+#define ICW3_SLAVE          0x02 /* And is cascading */
+
+#define ICW4                0x01 /* Deals with different modes */
 
 /* End-of-interrupt byte.  This gets OR'd with
  * the interrupt number and sent out to the PIC
