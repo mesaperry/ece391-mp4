717a4c8375655e29c05bbf4013ad878fcc067ba9
tkumar6 <tkumar6@illinois.edu>
2020-10-19 01:11:18 -0500

    updating exceptions and adding irqhandler placeholders

--- student-distrib/idt.c
+++ student-distrib/idt.c
@@ -1,46 +1,13 @@
+
 #include "idt.h"
+
 #include "x86_desc.h"
-#include "handlers.h"
 #include "lib.h"
 
 #define UPPER_MASK 0xffff0000 // selects upper memeory bits
 #define SYS_CALL   0x80        
 
 // assembly link declarations
-extern void sys_call();
-
-extern void excpt0();
-extern void excpt1();
-extern void excpt2();
-extern void excpt3();
-extern void excpt4();
-extern void excpt5();
-extern void excpt6();
-extern void excpt7();
-extern void excpt8();
-extern void excpt9();
-extern void excpt10();
-extern void excpt11();
-extern void excpt12();
-extern void excpt13();
-extern void excpt14();
-extern void excpt15();
-extern void excpt16();
-extern void excpt17();
-extern void excpt18();
-extern void excpt19();
-extern void excpt20();
-extern void excpt21();
-extern void excpt22();
-extern void excpt23();
-extern void excpt24();
-extern void excpt25();
-extern void excpt26();
-extern void excpt27();
-extern void excpt28();
-extern void excpt29();
-extern void excpt30();
-extern void excpt31();
 
 extern void irq0(); 
 extern void irq1(); 
@@ -67,6 +34,42 @@ void setup_idt_exceptions();
 void setup_idt_interrupts();
 void provisional_interrupt();
 void provisional_exception();
+
+void sys_call();
+void excpt0_handler();
+void excpt1_handler();
+void excpt2_handler();
+void excpt3_handler();
+void excpt4_handler();
+void excpt5_handler();
+void excpt6_handler();
+void excpt7_handler();
+void excpt8_handler();
+void excpt9_handler();
+void excpt10_handler();
+void excpt11_handler();
+void excpt12_handler();
+void excpt13_handler();
+void excpt14_handler();
+void excpt15_handler();
+void excpt16_handler();
+void excpt17_handler();
+void excpt18_handler();
+void excpt19_handler();
+void excpt20_handler();
+void excpt21_handler();
+void excpt22_handler();
+void excpt23_handler();
+void excpt24_handler();
+void excpt25_handler();
+void excpt26_handler();
+void excpt27_handler();
+void excpt28_handler();
+void excpt29_handler();
+void excpt30_handler();
+void excpt31_handler();
+
+// @TODO fix extern and assembly file
 
 /*
 *   provisional_interrupt
@@ -193,38 +196,38 @@ void setup_idt_exceptions()
     ///SET_IDT_ENTRY(idt[actual_int_num], exception());
     // for all exceptions
     
-    SET_IDT_ENTRY(idt[0], excpt0);          // divide_by_zero 
-    SET_IDT_ENTRY(idt[1], excpt1);          // Debug
-    SET_IDT_ENTRY(idt[2], excpt2);          // NMI Interrupt
-    SET_IDT_ENTRY(idt[3], excpt3);          // Breakpoint
-    SET_IDT_ENTRY(idt[4], excpt4);          // Overflow
-    SET_IDT_ENTRY(idt[5], excpt5);          // BOUND Range Exceeded 
-    SET_IDT_ENTRY(idt[6], excpt6);          // Invalid Opcode
-    SET_IDT_ENTRY(idt[7], excpt7);          // Device Not Available
-    SET_IDT_ENTRY(idt[8], excpt8);          // Double Fault
-    SET_IDT_ENTRY(idt[9], excpt9);          // Coprocessor Segment Overrun
-    SET_IDT_ENTRY(idt[10], excpt10);        // Invalid TSS
-    SET_IDT_ENTRY(idt[11], excpt11);        // Segment Not Present
-    SET_IDT_ENTRY(idt[12], excpt12);        // Stack Fault
-    SET_IDT_ENTRY(idt[13], excpt13);        // General Protection
-    SET_IDT_ENTRY(idt[14], excpt14);        // Page Fault
-    SET_IDT_ENTRY(idt[15], excpt15);        // Reserved for Intel
-    SET_IDT_ENTRY(idt[16], excpt16);        // Floating-Point Error
-    SET_IDT_ENTRY(idt[17], excpt17);        // Alignment Check
-    SET_IDT_ENTRY(idt[18], excpt18);        // Machine Check
-    SET_IDT_ENTRY(idt[19], excpt19);        // SIMD floating-point
-    SET_IDT_ENTRY(idt[20], excpt20);
-    SET_IDT_ENTRY(idt[21], excpt21);        // 21-19 Reserved for Intel
-    SET_IDT_ENTRY(idt[22], excpt22);
-    SET_IDT_ENTRY(idt[23], excpt23);
-    SET_IDT_ENTRY(idt[24], excpt24);
-    SET_IDT_ENTRY(idt[25], excpt25);
-    SET_IDT_ENTRY(idt[26], excpt26);
-    SET_IDT_ENTRY(idt[27], excpt27);
-    SET_IDT_ENTRY(idt[28], excpt28);
-    SET_IDT_ENTRY(idt[29], excpt29);
-    SET_IDT_ENTRY(idt[30], excpt30);         // Securtiy Explination
-    SET_IDT_ENTRY(idt[31], excpt31);         // Reserved for Intel
+    SET_IDT_ENTRY(idt[0], excpt0_handler);          // divide_by_zero 
+    SET_IDT_ENTRY(idt[1], excpt1_handler);          // Debug
+    SET_IDT_ENTRY(idt[2], excpt2_handler);          // NMI Interrupt
+    SET_IDT_ENTRY(idt[3], excpt3_handler);          // Breakpoint
+    SET_IDT_ENTRY(idt[4], excpt4_handler);          // Overflow
+    SET_IDT_ENTRY(idt[5], excpt5_handler);          // BOUND Range Exceeded 
+    SET_IDT_ENTRY(idt[6], excpt6_handler);          // Invalid Opcode
+    SET_IDT_ENTRY(idt[7], excpt7_handler);          // Device Not Available
+    SET_IDT_ENTRY(idt[8], excpt8_handler);          // Double Fault
+    SET_IDT_ENTRY(idt[9], excpt9_handler);          // Coprocessor Segment Overrun
+    SET_IDT_ENTRY(idt[10], excpt10_handler);        // Invalid TSS
+    SET_IDT_ENTRY(idt[11], excpt11_handler);        // Segment Not Present
+    SET_IDT_ENTRY(idt[12], excpt12_handler);        // Stack Fault
+    SET_IDT_ENTRY(idt[13], excpt13_handler);        // General Protection
+    SET_IDT_ENTRY(idt[14], excpt14_handler);        // Page Fault
+    SET_IDT_ENTRY(idt[15], excpt15_handler);        // Reserved for Intel
+    SET_IDT_ENTRY(idt[16], excpt16_handler);        // Floating-Point Error
+    SET_IDT_ENTRY(idt[17], excpt17_handler);        // Alignment Check
+    SET_IDT_ENTRY(idt[18], excpt18_handler);        // Machine Check
+    SET_IDT_ENTRY(idt[19], excpt19_handler);        // SIMD floating-point
+    SET_IDT_ENTRY(idt[20], excpt20_handler);
+    SET_IDT_ENTRY(idt[21], excpt21_handler);        // 21-19 Reserved for Intel
+    SET_IDT_ENTRY(idt[22], excpt22_handler);
+    SET_IDT_ENTRY(idt[23], excpt23_handler);
+    SET_IDT_ENTRY(idt[24], excpt24_handler);
+    SET_IDT_ENTRY(idt[25], excpt25_handler);
+    SET_IDT_ENTRY(idt[26], excpt26_handler);
+    SET_IDT_ENTRY(idt[27], excpt27_handler);
+    SET_IDT_ENTRY(idt[28], excpt28_handler);
+    SET_IDT_ENTRY(idt[29], excpt29_handler);
+    SET_IDT_ENTRY(idt[30], excpt30_handler);         // Securtiy Explination
+    SET_IDT_ENTRY(idt[31], excpt31_handler);         // Reserved for Intel
     
 }
 
@@ -260,3 +263,240 @@ void setup_idt_interrupts()
     SET_IDT_ENTRY(idt[47], irq15);
 }
 
+/*
+*   except#_handler() 
+*   IN: None
+*   OUT: exception message
+*   Description: Handler for exception, prints exception message and loops
+*   Side Effects: Prints to terminal and loops
+*
+*/
+
+void excpt0_handler()
+{
+    
+    printf("EXCEPTION0: Divide by Zero Error\n");
+    while(1);
+}
+
+void excpt1_handler()
+{
+    
+    printf("EXCEPTION1: Debug\n");
+    while(1);
+}
+
+void excpt2_handler()
+{
+    
+    printf("EXCEPTION2: NMI Interrupt\n");
+    while(1);
+}
+
+void excpt3_handler()
+{
+    
+    printf("EXCEPTION3: Breakpoint");
+    while(1);
+}
+
+void excpt4_handler()
+{
+    
+    printf("EXCEPTION4: Overflow");
+    while(1);
+}
+
+void excpt5_handler()
+{
+    
+    printf("EXCEPTION5: BOUND Range Exceeded");
+    while(1);
+}
+
+void excpt6_handler()
+{
+    
+    printf("EXCEPTION6: Invalid Opcode");
+    while(1);
+}
+
+void excpt7_handler()
+{
+    
+    printf("EXCEPTION7: Device Not Available");
+    while(1);
+}
+
+void excpt8_handler()
+{
+    
+    printf("EXCEPTION8: double fault");
+    while(1);
+}
+
+void excpt9_handler()
+{
+    
+    printf("EXCEPTION9: Coprocessor Segment Overrun");
+    while(1);
+}
+
+void excpt10_handler()
+{
+    
+    printf("EXCEPTION10: Invalid TSS");
+    while(1);
+}
+
+void excpt11_handler()
+{
+    
+    printf("EXCEPTION11: Segment Not Present");
+    while(1);
+}
+
+void excpt12_handler()
+{
+    
+    printf("EXCEPTION12: Stack Fault");
+    while(1);
+}
+
+void excpt13_handler()
+{
+    
+    printf("EXCEPTION13: General Protection");
+    while(1);
+}
+
+void excpt14_handler()
+{
+    
+    printf("EXCEPTION14: Page Fault");
+    while(1);
+}
+
+void excpt15_handler()
+{
+    
+    printf("EXCEPTION15: Reserved for Intel");
+    while(1);
+}
+
+void excpt16_handler()
+{
+    
+    printf("EXCEPTION16: Floating-Point Error");
+    while(1);
+}
+
+void excpt17_handler()
+{
+    
+    printf("EXCEPTION17: Alignment Check");
+    while(1);
+}
+
+void excpt18_handler()
+{
+    
+    printf("EXCEPTION18: Machine Check");
+    while(1);
+}
+
+void excpt19_handler()
+{
+    
+    printf("EXCEPTION19: SIMD Floating-Point");
+    while(1);
+}
+
+void excpt20_handler()
+{
+    
+    printf("EXCEPTION20: Virtualization Exception");
+    while(1);
+}
+
+void excpt21_handler()
+{
+    
+    printf("EXCEPTION21: Reserved for Intel");
+    while(1);
+}
+
+void excpt22_handler()
+{
+    
+    printf("EXCEPTION22: Reserved for Intel");
+    while(1);   
+}
+
+void excpt23_handler()
+{
+    
+    printf("EXCEPTION23: Reserved for Intel");
+    while(1);
+}
+
+void excpt24_handler()
+{
+    
+    printf("EXCEPTION24: Reserved for Intel");
+    while(1);
+}
+
+void excpt25_handler()
+{
+    
+    printf("EXCEPTION25: Reserved for Intel");
+    while(1);
+}
+
+void excpt26_handler()
+{
+    
+    printf("EXCEPTION26: Reserved for Intel");
+    while(1);
+}
+
+void excpt27_handler()
+{
+    
+    printf("EXCEPTION27: Reserved for Intel");
+    while(1);
+}
+
+void excpt28_handler()
+{
+    
+    printf("EXCEPTION28: Reserved for Intel");
+    while(1);
+}
+
+void excpt29_handler()
+{
+    
+    printf("EXCEPTION29: Reserved for Intel");
+    while(1);
+}
+
+void excpt30_handler()
+{
+    
+    printf("EXCEPTION30: Security Explination");
+    while(1);
+}
+
+void excpt31_handler()
+{
+    
+    printf("EXCEPTION31: Reserved for Intel");
+    while(1);
+}
+
+void sys_call(){
+    printf("A system call was made\n");
+    while(1);
+}
