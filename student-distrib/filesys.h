b9fdddad1e41cc7b28050277b95cfe12fd0f79ee
tkumar6 <tkumar6@illinois.edu>
2020-10-22 16:04:40 -0500

    added function sig in .h/preplanning

---
+++ student-distrib/filesys.h
@@ -0,0 +1,48 @@
+#ifndef _FILESYS_H
+#define _FILESYS_H
+#include "types.h"
+
+
+
+
+/* @TODO --------------
+*   DEFINE filesys struct?
+*   DEFINE inode type/struct?
+*   DEFINE dentry type/struct
+*   DEFINE data block type/struct
+*
+*/
+
+
+
+// filesystem
+/* Initializes filesystem */
+void init_fs();
+/*  */
+int32_t read_dentry_by_name (const uint8_t fileName, dentry_t* dentry);
+/*  */
+int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry);
+/*  */
+int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buffer, uint32_t length);
+
+// file operations
+/*  */
+int32_t file_read();
+/*  */
+int32_t file_write();
+/*  */
+int32_t file_open();
+/*  */
+int32_t file_close();
+
+// dir operations
+/*  */
+int32_t dir_read();
+/*  */
+int32_t dir_write();
+/*  */
+int32_t dir_open();
+/*  */
+int32_t dir_close();
+
+#endif //_FILESYS_H
