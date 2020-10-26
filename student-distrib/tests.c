9cf24343892832ffb78d654beb70ae56fa046851
Pieter Svenson <pieter2@illinois.edu>
2020-10-26 14:25:40 -0400

    moved tests

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -5,177 +5,9 @@
 #include "terminal.h"
 #include "filesys.h"
 #include "utils/char_util.h"
+#include "tests/checkpoint1.h"
 
 #include "rtc.h"
-
-#define PASS 1
-#define FAIL 0
-
-/* format these macros as you see fit */
-#define TEST_HEADER 	\
-	printf("[TEST %s] Running %s at %s:%d\n", __FUNCTION__, __FUNCTION__, __FILE__, __LINE__)
-#define TEST_OUTPUT(name, result)	\
-	printf("[TEST %s] Result = %s\n", name, (result) ? "PASS" : "FAIL");
-
-static inline void assertion_failure(){
-	/* Use exception #15 for assertions, otherwise
-	   reserved by Intel */
-	asm volatile("int $12");
-}
-
-
-/* Checkpoint 1 tests */
-
-/* IDT Test - whole table
- *
- * Asserts that first 10 IDT entries are not NULL
- * Inputs: None
- * Outputs: PASS/FAIL
- * Side Effects: None
- * Coverage: Load IDT, IDT definition
- * Files: x86_desc.h/S
- */
-int idt_test(){
-	TEST_HEADER;
-
-	int i;
-	int result = PASS;
-	for (i = 0; i < 256; ++i){
-		if ((idt[i].offset_15_00 == NULL) &&
-			(idt[i].offset_31_16 == NULL)){
-			assertion_failure();
-			result = FAIL;
-		}
-	}
-
-	return result;
-}
-
-/* Divide by Zero Exception Test
- *
- * Asserts divide by 0 Exception
- * Inputs: None
- * Outputs: PASS/FAIL
- * Side Effects: None
- * Coverage: Load IDT
- * Files: idt.c
- */
-int divide_0(){
-	int y = 0;
-	int x = 12;
-	x = x/y;
-	//assertion_failure();
-	return FAIL;
-}
-
-/* dereferencing null Exception Test
- *
- * Asserts Exception
- * Inputs: None
- * Outputs: PASS/FAIL
- * Side Effects: None
- * Coverage: Load IDT
- * Files: idt.c
- */
-int null_deref(){
-	int* y;
-	int x;
-	y = 0;
-	x = *(y);
-	//assertion_failure();
-	return FAIL;
-}
-
-/* bounds Exception Test
- *
- * Asserts bound range Exception
- * Inputs: None
- * Outputs: PASS/FAIL
- * Side Effects: None
- * Coverage: Load IDT
- * Files: idt.c
- */
-int bound_range(){
-	int y[2];
-	int x;
-	x = y[3];
-	//assertion_failure();
-	return FAIL;
-}
-
-int idt_woP(){
-	assertion_failure();
-	printf("1");
-	return 1;
-}
-
-/* term_write_test
- *
- * Print out characters passed in by buffer
- * Inputs: None
- * Outputs: output string
- * Side Effects: None
- * Coverage: terminal_write
- * Files: terminal.h/c
- */
-void term_write_test(){
-	uint32_t nbytes = 4;
-	uint8_t test_buffer[nbytes];
-	test_buffer[0] = 'a';
-	test_buffer[1] = 'b';
-	test_buffer[2] = 'c';
-	test_buffer[3] = 'd';
-
-	printf ("Input: abcd\n");
-	printf("Terminal prints: ");
-	uint32_t bytes_read = terminal_write(0, test_buffer, nbytes);
-
-	printf("\nNumber of bytes written: %d", bytes_read);
-
-}
-
-/* Keyboard test
- *
- * Checks if the keyboard functions
- * Inputs: None
- * Outputs: PASS/FAIL
- * Side Effects: None
- * Coverage: terminal_read, terminal_write
- * Files: terminal.h/c
- */
-int keyboard_test(){
-	int x = 0;
-	uint8_t buffer[MAX_BUFF_LENGTH];
-
-	while(1){
-		x = terminal_read(1, buffer, x);
-		terminal_write(1, buffer, x);
-	}
-	return 1;
-}
-
-int page_test()
-{
-	TEST_HEADER;
-	char _ignore0 __attribute__((unused)) = *(char*)VIDEO;
-	char _ignore1 __attribute__((unused)) = *(char*)KERNEL_MEMORY_ADDR;
-	return PASS;
-}
-
-/* These should fail */
-int force_page_exception_1()
-{
-	TEST_HEADER;
-	char _ignore __attribute__((unused)) = *(char*)(0x0);
-	return FAIL;
-}
-
-int force_page_exception_2()
-{
-	TEST_HEADER;
-	char _ignore __attribute__((unused)) = *(char*)(0xFFFFFFFF);
-	return FAIL;
-}
 
 /* Checkpoint 2 tests */
 
@@ -293,24 +125,14 @@ int test_read_files()
 {
 	TEST_HEADER;
 	uint8_t data[10000];
-	int count = 0;
-	
+
 	printf("frame0.txt:\n");
 	read_file_bytes_by_name((uint8_t*)"frame0.txt", data, 50);
 	print_buf(data, 50);
-<<<<<<< HEAD
 
 	printf("\n");
 
 	printf("frame1.txt:\n");
-=======
-	
-
-	printf("\n");
-	
-
-	printf("frame1.txt:\n");	
->>>>>>> c3304160540393a58afa91f64c6ce5656bd2ece1
 	if (read_file_bytes_by_name((uint8_t*)"frame1.txt", data, 500) != -1)
 	{
 		print_buf(data, 500);
@@ -318,10 +140,6 @@ int test_read_files()
 
 
 	printf("\n\n");
-<<<<<<< HEAD
-=======
-	
->>>>>>> c3304160540393a58afa91f64c6ce5656bd2ece1
 
 	printf("fish:\n");
 	if (read_file_bytes_by_name((uint8_t*)"fish", data, 4500) != -1)
@@ -329,35 +147,23 @@ int test_read_files()
 
 		print_buf(data, 4500);
 	}
-<<<<<<< HEAD
 
 	printf("\n");
-=======
-	
-	
-	printf("\n");
-	
->>>>>>> c3304160540393a58afa91f64c6ce5656bd2ece1
 
 	printf("verlargetextwithverylongname.tx(t):\n");
 	if (read_file_bytes_by_name((uint8_t*)"verylargetextwithverylongname.txt", data, 10000) != -1) print_buf(data, 10000);
 
 
 	printf("\n");
-	
-	
+
+
 	printf("grep:\n");
-<<<<<<< HEAD
 
 	if (read_file_bytes_by_name((uint8_t*)"grep", data, 5000) != -1) print_buf(data, 5000);
 
-=======
-	if (read_file_bytes_by_name((uint8_t*)"grep", data, 5000) != -1) print_buf(data, 5000); 
-	
->>>>>>> c3304160540393a58afa91f64c6ce5656bd2ece1
 
 	printf("\n");
-	
+
 
 	printf("ls:\n");
 	if (read_file_bytes_by_name((uint8_t*)"ls", data, 5000) != -1) print_buf(data, 5000);
@@ -440,18 +246,7 @@ int test_write()
 
 /* Test suite entry point */
 void launch_tests(){
-	//TEST_OUTPUT("idt_test", idt_test());
-	//TEST_OUTPUT("Deref video and kernel memory", page_test());
-	// TEST_OUTPUT("Force page exception 1", force_page_exception_1());
-	// TEST_OUTPUT("Force page exception 2", force_page_exception_2());
-	//TEST_OUTPUT("divide by zero", divide_0());
-	//TEST_OUTPUT("dereference null", null_deref());
-	//TEST_OUTPUT("bounds range", bound_range());
-	//TEST_OUTPUT("pf", pf());
-	// launch your tests here
-	//TEST_OUTPUT("IDT without paging", idt_woP());
-	//TEST_OUTPUT("keybord", );
-	// keyboard_test();
+	test_all_checkpoint1();
 
 	/* start checkpoint 2*/
 	// TEST_OUTPUT("print all filesys", print_all_filesys());
---
+++ student-distrib/tests/checkpoint1.h
@@ -0,0 +1,3 @@
+/* All exposed test cases for checkpoint 1 */
+
+void test_all_checkpoint1();
