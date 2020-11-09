e45f88fe36fbe091d68631f88694d1e93b66ccf3
Pieter Svenson <pieter2@illinois.edu>
2020-11-08 19:24:38 -0500

    Blocking out execute syscall

--- student-distrib/syscalls.c
+++ student-distrib/syscalls.c
@@ -1,258 +1,287 @@
-// /* Created by Team 11 */
-// #include "syscalls.h"
-// #include "paging.h"
-// #include "lib.h"
-// #include "x86_desc.h"
-// #include "terminal.h"
+/* Created by Team 11 */
+#include "syscalls.h"
+#include "paging.h"
+#include "lib.h"
+#include "x86_desc.h"
+#include "terminal.h"
 
-// /* File operation structs */
-// fops_t terminal_funcs =
-// {
-// 	.read = terminal_read,
-// 	.write = terminal_write,
-// 	.open = terminal_open,
-// 	.close = terminal_close
-// };
+/* File operation structs */
+fops_t terminal_funcs =
+{
+	.read = terminal_read,
+	.write = terminal_write,
+	.open = terminal_open,
+	.close = terminal_close
+};
 
-// fops_t rtc_funcs =
-// {
-// 	.read = rtc_read,
-// 	.write = rtc_write,
-// 	.open = rtc_open,
-// 	.close = rtc_close
-// };
+fops_t rtc_funcs =
+{
+	.read = rtc_read,
+	.write = rtc_write,
+	.open = rtc_open,
+	.close = rtc_close
+};
 
-// fops_t fsys_funcs =
-// {
-// 	.read = file_read,
-// 	.write = file_write,
-// 	.open = file_open,
-// 	.close = file_close
-// };
+fops_t fsys_funcs =
+{
+	.read = file_read,
+	.write = file_write,
+	.open = file_open,
+	.close = file_close
+};
 
-// fops_t dir_funcs =
-// {
-// 	.read = directory_read,
-// 	.write = dir_write,
-// 	.open = dir_open,
-// 	.close = dir_close
-// };
+fops_t dir_funcs =
+{
+	.read = directory_read,
+	.write = dir_write,
+	.open = dir_open,
+	.close = dir_close
+};
 
-// boot_block_t* boot_blk;
-// int p_id = 1;
-// static int32_t procs[MAX_DEVICES + 1] = {0};
+int p_id = 1;
+static int32_t procs[MAX_DEVICES + 1] = {0};
 
-// /*
-// * add_process()
-// * DESCRIPTION: Adds a new process and returns its id if there's room
-// * INPUTS: None
-// * OUTPUT: returns process id on success, -1 on failure
-// */
-// int32_t add_process(){
-//     uint32_t i;
-//     for(i = 1; i < MAX_DEVICES + 1; i++){
-//         if(procs[i] == 0){
-//             procs[i] = 1;
-//             return i;
-//         }
-//     }
-//     return -1;
-// }
+/*
+* add_process()
+* DESCRIPTION: Adds a new process and returns its id if there's room
+* INPUTS: None
+* OUTPUT: returns process id on success, -1 on failure
+*/
+int32_t add_process(){
+    uint32_t i;
+    for(i = 1; i < MAX_DEVICES + 1; i++){
+        if(procs[i] == 0){
+            procs[i] = 1;
+            return i;
+        }
+    }
+    return -1;
+}
 
-// /*
-// * delete_process(int32_t pid)
-// * DESCRIPTION: Removes a specified process
-// * INPUTS: pid
-// * OUTPUT: returns 0 on success, -1 on failure
-// */
-// int32_t delete_process(int32_t p_id){
-//     if(p_id < 0 || p_id >= MAX_DEVICES){
-//         return -1;
-//     }
-//     procs[p_id] = 0;
-//     return 0;
-// }
+/*
+* delete_process(int32_t pid)
+* DESCRIPTION: Removes a specified process
+* INPUTS: pid
+* OUTPUT: returns 0 on success, -1 on failure
+*/
+int32_t delete_process(int32_t p_id){
+    if(p_id < 0 || p_id >= MAX_DEVICES){
+        return -1;
+    }
+    procs[p_id] = 0;
+    return 0;
+}
 
+int32_t halt (uint8_t status)
+{
 
-// /*
-// * int32_t read(int32_t fd, const uint8_t * buf, int32_t nbytes);
-// * DESCRIPTION: calls the read function for given file
-// * INPUTS: fd - file descriptor
-// *         buffer
-// *         nbytes - number of bytes
-// * OUTPUT: Return -1 on fail
-// *         Return corresponding function
-// */
-// int32_t read(int32_t fd, void* buf, int32_t nbytes)
-// {
-//     /* Get current process control block from esp */
-//     pcb_t* curr = get_PCB();
+}
 
-//     /* Check for valid fd */
-//     /* fd = 1 is write only */
-//     if(buf == NULL || fd < 0 || fd > MAX_FD || fd == 1)
-//     {
-//         return -1;
-//     }
+int32_t execute (const uint8_t* command)
+{
+	/* Get first word (command) */
 
-//     /* check if fd has been opened */
-//     if(curr->file_array[fd].flags == 0)
-//     {
-//         return -1;
-//     }
+	/* Check that command is an executable */
 
-//     /* Return a call to the specified function */
-//     return (curr)->file_array[fd].fops->read(fd, buf, nbytes);
-// }
+	/* Save extra parameters to globa variable, stripped of leading spaces */
 
-// /*
-// * int32_t write(int32_t fd, const uint8_t * buf, int32_t nbytes);
-// * DESCRIPTION: calls the write function for rtc, file, or terminal
-// * INPUTS: fd - file descriptor
-// *         buffer
-// *         nbytes - number of bytes
-// * OUTPUT: Return -1 on fail
-// *         Return corresponding function
-// */
-// int32_t write(int32_t fd, const void * buf, int32_t nbytes)
-// {
-// 		/* Get current process control block from esp */
-//     pcb_t* curr = get_PCB();
+	/* Paging: load user level program loaded in page starting at 128MB */
+	/*           and physical memory starts at 8MB + (pid * 4MB)        */
 
-//     /* Check for valid fd */
-//     /* fd = 0 is read only */
-//     if(buf == NULL || fd < 0 || fd > MAX_FD || fd == 0)
-//     {
-//         return -1;
-//     }
+	// map_v_p(128MB + ..., 8MB + ..., 1)
 
-//     /* check if fd has been opened */
-//     if(curr->file_array[fd].flags == 0)
-//     {
-//         return -1;
-//     }
+	/* process id starts at 0?? */
 
-//     /* Return a call to the specified function */
-//     return (curr)->file_array[fd].fops->write(fd, buf, nbytes);
-// }
+	/* User Level program loading:                               */
+	/*   Copy file contents to correct location                  */
+	/*   Find the first instruction's address                    */
 
-// /*
-// * int32_t open(uint8_t* filename);
-// * DESCRIPTION: calls the open function for rtc, file, or terminal
-// * INPUTS: fd - file descriptor
-// *         buffer
-// *         nbytes - number of bytes
-// * OUTPUT: Return -1 on fail
-// *         Return corresponding function
-// */
-// int32_t open(const uint8_t* filename)
-// {
-// 		/* Initialize variables */
-//     int32_t fd = -1;
-//     dentry_t dentry;
-//     fd_t* fd_ptr = NULL;
-// 		uint32_t index;
+	/* Create PCB */
 
-// 		/* Get current PCB */
-// 		pcb_t* pcb = get_PCB();
+	/* Context Switch */
 
-//     for(index = 2; index < FILE_ARRAY_LEN; index++)
-// 		{
-// 				/* Find an unused file in file_array */
-//         if(pcb->file_array[index].flags == 0 && fd < 2)
-// 				{
-//             fd = index;
-//             break;
-//         }
-//     }
+	return -1;
+}
 
-// 		/* Return -1, if fd never set and all files in use */
-//     if(fd == -1)
-// 		{
-//         return fd;
-//     }
+/*
+* int32_t read(int32_t fd, const uint8_t * buf, int32_t nbytes);
+* DESCRIPTION: calls the read function for given file
+* INPUTS: fd - file descriptor
+*         buffer
+*         nbytes - number of bytes
+* OUTPUT: Return -1 on fail
+*         Return corresponding function
+*/
+int32_t read(int32_t fd, void* buf, int32_t nbytes)
+{
+    /* Get current process control block from esp */
+    pcb_t* curr = get_PCB();
 
-// 		// Get pointer to the current file_array
-//     fd_ptr = &(pcb->file_array[fd]);
+    /* Check for valid fd */
+    /* fd = 1 is write only */
+    if(buf == NULL || fd < 0 || fd > MAX_FD || fd == 1)
+    {
+        return -1;
+    }
 
-// 		/* Get dentry based on filename */
-//     if (read_dentry_by_name(filename, &dentry) == -1)
-// 		{
-//         return -1;
-//     }
+    /* check if fd has been opened */
+    if(curr->file_array[fd].flags == 0)
+    {
+        return -1;
+    }
 
-// 		/* Handle different file types */
-//     if(dentry.file_type == 0) // RTC File
-// 		{
-//         fd_ptr->inode = NULL;
-//         fd_ptr->pos = 0;
-//         fd_ptr->flags = 1;
-//         fd_ptr->fops = &rtc_funcs;
-//     }
-//     else if(dentry.file_type == 1) // Directory
-// 		{
-//         fd_ptr->inode = NULL;
-//         fd_ptr->pos = 0;
-//         fd_ptr->flags = 1;
-//         fd_ptr->fops = &dir_funcs;
-//     }
-//     else if(dentry.file_type == 2)// Regular File
-// 		{
-//         fd_ptr->inode = dentry.inode_num;
-//         fd_ptr->pos = 0;
-//         fd_ptr->flags = 1;
-//         fd_ptr->fops = &fsys_funcs;
-//     }
-//     else
-// 		{
-//         return -1;
-//     }
+    /* Return a call to the specified function */
+    return (curr)->file_array[fd].fops->read(fd, buf, nbytes);
+}
 
-// 		/* Return fd that's been opened */
-//     return fd;
-// }
+/*
+* int32_t write(int32_t fd, const uint8_t * buf, int32_t nbytes);
+* DESCRIPTION: calls the write function for rtc, file, or terminal
+* INPUTS: fd - file descriptor
+*         buffer
+*         nbytes - number of bytes
+* OUTPUT: Return -1 on fail
+*         Return corresponding function
+*/
+int32_t write(int32_t fd, const void * buf, int32_t nbytes)
+{
+		/* Get current process control block from esp */
+    pcb_t* curr = get_PCB();
 
-// /*
-// * int32_t close(int32_t fd);
-// * DESCRIPTION: calls the close function for rtc, file, or terminal
-// * INPUTS: fd - file descriptor
-// *         buffer
-// *         nbytes - number of bytes
-// * OUTPUT: Return -1 on fail
-// *         Return corresponding function
-// */
-// int32_t close(int32_t fd)
-// {
-// 		/* Get PCB of current process block */
-//     pcb_t* curr = find_PCB(p_id);
+    /* Check for valid fd */
+    /* fd = 0 is read only */
+    if(buf == NULL || fd < 0 || fd > MAX_FD || fd == 0)
+    {
+        return -1;
+    }
 
-//     /* Check if descriptor is valid and file is in use */
-//     if(fd < MIN_FD || fd > MAX_FD || curr->file_array[fd].flags == 0)
-// 		{
-//         return -1;
-//     }
+    /* check if fd has been opened */
+    if(curr->file_array[fd].flags == 0)
+    {
+        return -1;
+    }
 
-//     /* Return fail if close function is unsuccessful */
-//     if ((curr)->file_array[fd].fops->close(fd) != 0)
-// 		{
-//         return -1;
-//     }
+    /* Return a call to the specified function */
+    return (curr)->file_array[fd].fops->write(fd, buf, nbytes);
+}
 
-// 		/* Re-set file with default values */
-//     curr->file_array[fd].fops = NULL;
-//     curr->file_array[fd].pos = 0;
-//     curr->file_array[fd].flags = 0;
+/*
+* int32_t open(uint8_t* filename);
+* DESCRIPTION: calls the open function for rtc, file, or terminal
+* INPUTS: fd - file descriptor
+*         buffer
+*         nbytes - number of bytes
+* OUTPUT: Return -1 on fail
+*         Return corresponding function
+*/
+int32_t open(const uint8_t* filename)
+{
+		/* Initialize variables */
+    int32_t fd = -1;
+    dentry_t dentry;
+    fd_t* fd_ptr = NULL;
+		uint32_t index;
 
-// 		/* Return 0 on success */
-//     return 0;
-// }
+		/* Get current PCB */
+		pcb_t* pcb = get_PCB();
 
-// /*
-// * Find_PCB()
-// * DESCRIPTION: Finds the PCB of the given p_id
-// * INPUTS: p_id
-// * OUTPUT: Returns the PCB
-// */
-// pcb_t* find_PCB(int p_id) {
-//     return (pcb_t*)(MB_8 - KB_8 * (p_id + 1));
-// }
+    for(index = 2; index < FILE_ARRAY_LEN; index++)
+		{
+				/* Find an unused file in file_array */
+        if(pcb->file_array[index].flags == 0 && fd < 2)
+				{
+            fd = index;
+            break;
+        }
+    }
+
+		/* Return -1, if fd never set and all files in use */
+    if(fd == -1)
+		{
+        return fd;
+    }
+
+		// Get pointer to the current file_array
+    fd_ptr = &(pcb->file_array[fd]);
+
+		/* Get dentry based on filename */
+    if (read_dentry_by_name(filename, &dentry) == -1)
+		{
+        return -1;
+    }
+
+		/* Handle different file types */
+    if(dentry.file_type == 0) // RTC File
+		{
+        fd_ptr->inode = NULL;
+        fd_ptr->pos = 0;
+        fd_ptr->flags = 1;
+        fd_ptr->fops = &rtc_funcs;
+    }
+    else if(dentry.file_type == 1) // Directory
+		{
+        fd_ptr->inode = NULL;
+        fd_ptr->pos = 0;
+        fd_ptr->flags = 1;
+        fd_ptr->fops = &dir_funcs;
+    }
+    else if(dentry.file_type == 2)// Regular File
+		{
+        fd_ptr->inode = dentry.inode_num;
+        fd_ptr->pos = 	0;
+        fd_ptr->flags = 1;
+        fd_ptr->fops = &fsys_funcs;
+    }
+    else
+		{
+        return -1;
+    }
+
+		/* Return fd that's been opened */
+    return fd;
+}
+
+/*
+* int32_t close(int32_t fd);
+* DESCRIPTION: calls the close function for rtc, file, or terminal
+* INPUTS: fd - file descriptor
+*         buffer
+*         nbytes - number of bytes
+* OUTPUT: Return -1 on fail
+*         Return corresponding function
+*/
+int32_t close(int32_t fd)
+{
+		/* Get PCB of current process block */
+    pcb_t* curr = find_PCB(p_id);
+
+    /* Check if descriptor is valid and file is in use */
+    if(fd < MIN_FD || fd > MAX_FD || curr->file_array[fd].flags == 0)
+		{
+        return -1;
+    }
+
+    /* Return fail if close function is unsuccessful */
+    if ((curr)->file_array[fd].fops->close(fd) != 0)
+		{
+        return -1;
+    }
+
+		/* Re-set file with default values */
+    curr->file_array[fd].fops = NULL;
+    curr->file_array[fd].pos = 0;
+    curr->file_array[fd].flags = 0;
+
+		/* Return 0 on success */
+    return 0;
+}
+
+/*
+* Find_PCB()
+* DESCRIPTION: Finds the PCB of the given p_id
+* INPUTS: p_id
+* OUTPUT: Returns the PCB
+*/
+pcb_t* find_PCB(int p_id) {
+    return (pcb_t*)(MB_8 - KB_8 * (p_id + 1));
+}
