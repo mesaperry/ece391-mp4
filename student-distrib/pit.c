2dbad9fccf75aaec169f21a4c2ed2ca5af998db9
Perry <mesagp2@illinois.edu>
2020-12-07 16:29:40 -0600

    PIT magic numbers

--- student-distrib/pit.c
+++ student-distrib/pit.c
@@ -10,6 +10,14 @@
 #define INT_INTERVAL                 15 // timer interrupt interval in ms
 #define PIT_IRQ                     0x00
 
+#define MAX_FREQ                    1193182
+#define MS_IN_SEC                   1000
+#define PIT_MODE3                   0x36
+#define PIT_CH0                     0x40
+#define PIT_CMD                     0x43
+#define BYTE_MASK                   0xFF
+#define BYTE_SIZE                   8
+
 
 void pit_handler() {
     if (cycle_task()) {
@@ -27,11 +35,11 @@ void pit_handler() {
  *  SIDE EFFECTS: enables the PIT on the PIC and give a starting frequency
  */
 void pit_init() {
-    uint16_t divisor = 1193182 * INT_INTERVAL / 1000;
+    uint16_t divisor = MAX_FREQ * INT_INTERVAL / MS_IN_SEC;
 
-    outb(0x36, 0x43);
-    outb((uint8_t) (divisor & 0xFF), 0x40);
-    outb((uint8_t) ((divisor >> 8) & 0xFF), 0x40);
+    outb(PIT_MODE3, PIT_CMD);
+    outb((uint8_t) (divisor & BYTE_MASK), PIT_CH0);
+    outb((uint8_t) ((divisor >> BYTE_SIZE) & BYTE_MASK), PIT_CH0);
 
     enable_irq(PIT_IRQ);
 }
